43e01e30b83ff71aa6b5a1a9f5983028bdc0cda6
Brainless-Loco <dev.tcdas@gmail.com>
2024-09-21 04:26:50 +0600

    Assignment 1 Task 3 done

---
+++ Assignment/Assignment 1/includes/footer.php
@@ -0,0 +1,5 @@
+
+<footer class="bg-dark text-white text-center p-3">
+    <p>&copy; 2024 Your Name. All rights reserved.</p>
+</footer>
+
---
+++ Assignment/Assignment 1/includes/header.php
@@ -0,0 +1,11 @@
+
+    <header class="container-fluid bg-dark text-white p-3">
+        <div class="d-flex justify-content-between align-items-center">
+            <div class="logo">LOGO</div>
+            <nav>
+                <a href="index.php" class="text-white mx-3">Home</a>
+                <a href="info.php" class="text-white mx-3">Info</a>
+                <a href="mailto:yourname@email.com" class="text-white mx-3">Email Me</a>
+            </nav>
+        </div>
+    </header>
